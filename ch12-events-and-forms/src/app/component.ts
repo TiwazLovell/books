072baa9a3426f94e86ca3a3ce07ea9d722d16bec
Leonardo Martinas <p3701660@ness.com>
2024-02-22 16:08:02 +0200

    ch12 - initiate the component and the template

---
+++ ch12-events-and-forms/src/app/component.ts
@@ -0,0 +1,21 @@
+import { ApplicationRef, Component } from "@angular/core";
+import { Model } from "./repository.mode";
+import { Product } from "./product.model";
+
+@Component({
+    selector: "app",
+    templateUrl: "template.html"
+})
+export class ProductComponent {
+    model: Model = new Model();
+
+    getProduct(key: number): Product | undefined {
+        return this.model.getProduct(key);
+    }
+
+    getProducts(): Product[] {
+        return this.model.getProducts();
+    }
+
+    selectedProduct: string | undefined;
+}
