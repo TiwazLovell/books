d1428c1a71d0d245a237aed99666814eb81dcd12
Leonardo Martinas <p3701660@ness.com>
2024-03-01 15:28:16 +0200

    ch13 - data-bound input properties (ngOnInit)

--- ch13-attribute-directives/src/app/attr.directive.ts
+++ ch13-attribute-directives/src/app/attr.directive.ts
@@ -1,11 +1,17 @@
-import { Directive, ElementRef, Attribute } from "@angular/core";
+import { Directive, ElementRef, Input } from "@angular/core";
 
 @Directive({
-    selector: "[pa-attr]",
+    selector: "[pa-attr]"
 })
 export class PaAttrDirective {
 
-    constructor(element: ElementRef, @Attribute("pa-attr") bgClass: string) {
-        element.nativeElement.classList.add(bgClass || "table-success", "fw-bold");
+    constructor(private element: ElementRef) { }
+
+    @Input("pa-attr")
+    bgClass: string | null = "";
+
+    ngOnInit() {
+        this.element.nativeElement.classList.add(this.bgClass || "table-success",
+            "fw-bold");
     }
 }
