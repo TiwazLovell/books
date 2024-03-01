650adde12345c51c322ba89415f4d5fd693a7588
Leonardo Martinas <p3701660@ness.com>
2024-03-01 15:41:22 +0200

    ch13 - responding to input property changes (ngOnChanges)

--- ch13-attribute-directives/src/app/attr.directive.ts
+++ ch13-attribute-directives/src/app/attr.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input } from "@angular/core";
+import { Directive, ElementRef, Input, SimpleChanges } from "@angular/core";
 
 @Directive({
     selector: "[pa-attr]"
@@ -10,8 +10,14 @@ export class PaAttrDirective {
     @Input("pa-attr")
     bgClass: string | null = "";
 
-    ngOnInit() {
-        this.element.nativeElement.classList.add(this.bgClass || "table-success",
-            "fw-bold");
+    ngOnChanges(changes: SimpleChanges) {
+        let change = changes["bgClass"];
+        let classList = this.element.nativeElement.classList;
+        if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+            classList.remove(change.previousValue);
+        }
+        if (!classList.contains(change.currentValue)) {
+            classList.add(change.currentValue);
+        }
     }
 }
