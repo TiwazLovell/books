db198f8403d8b4edd9f46fcf4647f7d63edfe7de
Leonardo Martinas <p3701660@ness.com>
2024-03-04 16:49:22 +0200

    ch15 - created the sample project

---
+++ ch15-components/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { ProductComponent } from './component';
+import { FormsModule } from "@angular/forms";
+import { PaAttrDirective } from "./attr.directive";
+import { PaModel } from './twoway.directive';
+import { PaStructureDirective } from './structure.directive';
+import { PaIteratorDirective } from './iterator.directive';
+import { PaCellColor } from './cellColor.directive';
+import { PaCellColorSwitcher } from './cellColorSwitcher.directive';
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    PaAttrDirective,
+    PaModel,
+    PaStructureDirective,
+    PaIteratorDirective,
+    PaCellColor,
+    PaCellColorSwitcher
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule { }
