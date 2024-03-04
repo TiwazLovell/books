dfa43d4fbbf3a98d00f7c69c14f4a595d604dff4
Leonardo Martinas <p3701660@ness.com>
2024-03-04 10:26:22 +0200

    ch14 - creating, implementing & enabling a structural directive (ngIf)

--- ch14-structural-directives/src/app/app.module.ts
+++ ch14-structural-directives/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ProductComponent } from './component';
 import { FormsModule } from "@angular/forms";
 import { PaAttrDirective } from "./attr.directive";
 import { PaModel } from './twoway.directive';
+import { PaStructureDirective } from './structure.directive';
 
 @NgModule({
   declarations: [
     ProductComponent,
     PaAttrDirective,
     PaModel,
+    PaStructureDirective
   ],
   imports: [
     BrowserModule,
