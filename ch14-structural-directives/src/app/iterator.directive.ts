74e77f788236120ba6fdeba8d9054b8e8170c7a9
Leonardo Martinas <p3701660@ness.com>
2024-03-04 12:39:33 +0200

    ch14 - collection-level data changes

--- ch14-structural-directives/src/app/iterator.directive.ts
+++ ch14-structural-directives/src/app/iterator.directive.ts
@@ -1,26 +1,42 @@
-import { Directive, ViewContainerRef, TemplateRef, Input }
+import {
+    Directive, ViewContainerRef, TemplateRef, Input,
+    IterableDiffer, IterableDiffers, IterableChangeRecord
+}
     from "@angular/core";
 
 @Directive({
     selector: "[paForOf]"
 })
 export class PaIteratorDirective {
+    private differ: IterableDiffer<any> | undefined;
 
     constructor(private container: ViewContainerRef,
-        private template: TemplateRef<Object>) { }
+        private template: TemplateRef<Object>,
+        private differs: IterableDiffers) { }
 
     @Input("paForOf")
     dataSource: any;
 
     ngOnInit() {
-        this.container.clear();
-        for (let i = 0; i < this.dataSource.length; i++) {
-            this.container.createEmbeddedView(this.template,
-                new PaIteratorContext(this.dataSource[i],
-                    i, this.dataSource.length));
-        }
+        this.differ =
+            <IterableDiffer<any>>this.differs.find(this.dataSource).create();
     }
 
+    ngDoCheck() {
+        let changes = this.differ?.diff(this.dataSource);
+        if (changes != null) {
+            console.log("ngDoCheck called, changes detected");
+            let arr: IterableChangeRecord<any>[] = [];
+            changes.forEachAddedItem(addition => arr.push(addition));
+            arr.forEach(addition => {
+                if (addition.currentIndex != null) {
+                    this.container.createEmbeddedView(this.template,
+                        new PaIteratorContext(addition.item, addition.currentIndex,
+                            arr.length));
+                }
+            });
+        }
+    }
 }
 
 class PaIteratorContext {
@@ -33,12 +49,5 @@ class PaIteratorContext {
         this.even = !this.odd;
         this.first = index == 0;
         this.last = index == total - 1;
-
-        setInterval(() => {
-            this.odd = !this.odd; this.even = !this.even;
-            this.$implicit.price++;
-        }, 2000);
     }
-
-
 }
