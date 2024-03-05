ec7b680c238ec3d1e61a0ecde56ea7df33ca07db
Leonardo Martinas <p3701660@ness.com>
2024-03-05 19:46:41 +0200

    ch16 - formatting numbers (number)

--- ch16-pipes/src/app/app.module.ts
+++ ch16-pipes/src/app/app.module.ts
@@ -14,6 +14,10 @@ import { ProductFormComponent } from './productForm.component';
 import { PaToggleView } from './toggleView.component';
 import { PaAddTaxPipe } from './addTax.pipe';
 import { PaCategoryFilterPipe } from './categoryFilter.pipe';
+import { LOCALE_ID } from "@angular/core";
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeFr);
 
 
 @NgModule({
@@ -35,7 +39,7 @@ import { PaCategoryFilterPipe } from './categoryFilter.pipe';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: "fr-FR" }],
   bootstrap: [ProductComponent]
 })
 export class AppModule { }
